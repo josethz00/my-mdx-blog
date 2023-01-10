1f5c6ccf5fb5c88499f2033754f93567407dd9eb
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-10 11:03:28 -0300

    chore: add link preview example

--- pages/interesting-links.tsx
+++ pages/interesting-links.tsx
@@ -1,3 +1,4 @@
+import { ReactTinyLink } from "react-tiny-link";
 import Layout from "../components/Layout";
 
 const InterestingLinks = (): JSX.Element => {
@@ -5,8 +6,15 @@ const InterestingLinks = (): JSX.Element => {
     <Layout>
       <h1 className="text-3xl font-bold">Interesting Links</h1>
       <p className="mt-4 text-gray-600 dark:text-gray-400">
-        Here are some interesting links that I've found on the internet.
+        Here are some interesting links that I&apos;ve found on the internet.
       </p>
+      <ReactTinyLink
+        cardSize="small"
+        showGraphic={true}
+        maxLine={2}
+        minLine={1}
+        url="https://emanuelferreira.substack.com/p/how-to-be-antifragile-for-developers"
+      />
     </Layout>
   )
 };
