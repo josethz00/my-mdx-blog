3ec1de8f7b433a09957634cd076f9e0e7642c353
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-01-10 10:08:28 -0300

    chore: change meta name title

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,7 +5,7 @@ export const About = (): JSX.Element => {
   return (
     <Layout
       customMeta={{
-        title: 'About - Hunter Chang',
+        title: 'About - Jose Thomaz',
       }}
     >
       <h1>About Page</h1>
